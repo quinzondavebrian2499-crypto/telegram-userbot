a3bc973d959d0b1ee8baee39c5b13ec4124c6715
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-03-13 16:02:56 +0800

    Update setprefix.py

--- plugins/setprefix.py
+++ plugins/setprefix.py
@@ -1,11 +1,16 @@
 from telethon import events
 from __main__ import client
+import database
 import prefix
 
+
 @client.on(events.NewMessage(pattern=r"\.setprefix (.+)"))
-async def set_prefix(event):
+async def setprefix(event):
+
     new_prefix = event.pattern_match.group(1)
+
+    database.set("prefix", new_prefix)
 
     prefix.PREFIX = new_prefix
 
-    await event.reply(f"✅ Command prefix changed to: {new_prefix}")
+    await event.reply(f"✅ Prefix changed to: {new_prefix}")
