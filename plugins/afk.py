c3d255711deb79415788ab188d4cff0ac08348c0
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-03-13 16:36:16 +0800

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -1,29 +1,15 @@
-from telethon import events
-from __main__ import client
+# plugin: AFK | command: afk | desc: Set AFK status
+
+from command import cmd
 
 AFK = False
-AFK_REASON = ""
 
-@client.on(events.NewMessage(pattern=r".afk(?: (.*))?"))
-async def set_afk(event):
-    global AFK, AFK_REASON
+
+@cmd("afk")
+async def afk(event):
+
+    global AFK
 
     AFK = True
-    AFK_REASON = event.pattern_match.group(1) or "AFK"
 
-    await event.reply(f"😴 AFK Enabled\nReason: {AFK_REASON}")
-
-@client.on(events.NewMessage(incoming=True))
-async def afk_reply(event):
-    global AFK
-
-    if AFK and not event.out:
-        await event.reply(f"😴 I am AFK\nReason: {AFK_REASON}")
-
-@client.on(events.NewMessage(outgoing=True))
-async def remove_afk(event):
-    global AFK
-
-    if AFK:
-        AFK = False
-        await event.reply("✅ AFK Disabled")
+    await event.reply("😴 AFK Enabled")
