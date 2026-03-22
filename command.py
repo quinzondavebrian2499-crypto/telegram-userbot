9e05229264cb22571292d84db4045e512859d516
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-03-22 12:02:48 +0800

    Update command.py

--- command.py
+++ command.py
@@ -1,27 +1,30 @@
 from telethon import events
-from config import PREFIX, OWNER_ID
+from config import OWNER_ID, get_prefix
 from __main__ import client
 import re
 
 
 def cmd(name):
 
-    # STRICT pattern
-    pattern = rf"^{re.escape(PREFIX)}{name}(?:\s|$)"
-
     def decorator(func):
 
         async def wrapper(event):
 
-            # OWNER ONLY
             if event.sender_id != OWNER_ID:
+                return
+
+            prefix = get_prefix()
+
+            pattern = rf"^{re.escape(prefix)}{name}(?:\s|$)"
+
+            if not re.match(pattern, event.raw_text):
                 return
 
             await func(event)
 
         client.add_event_handler(
             wrapper,
-            events.NewMessage(pattern=pattern)
+            events.NewMessage(incoming=True)
         )
 
         return wrapper
