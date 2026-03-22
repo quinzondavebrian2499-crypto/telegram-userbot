3c52899cdf5a64db3769877d5e2413ab244b2ddd
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-03-22 12:12:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
+import os
 import database
 
+# Telegram API
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+
+# Owner
 OWNER_ID = 1327719064  # your Telegram ID
+
+
+# ======================
+# PREFIX SYSTEM
+# ======================
 
 def get_prefix():
     return database.get("prefix", ".")
+
 
 def set_prefix(p):
     database.set("prefix", p)
