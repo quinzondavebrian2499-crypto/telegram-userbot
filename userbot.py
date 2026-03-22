295d578499b6e49238ef8a65df80f8ac1fd17918
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-03-22 08:28:00 +0800

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,59 +1,17 @@
-from telethon import TelegramClient, events
-import os
+from telethon import TelegramClient
 import importlib
+import os
+from config import API_ID, API_HASH
 
-# =========================
-# TELEGRAM API
-# =========================
+client = TelegramClient("userbot", API_ID, API_HASH)
 
-api_id = 2127709
-api_hash = "5b7a6b1e4ff43ac9437114de58cef95a"
-
-client = TelegramClient("userbot", api_id, api_hash)
-
-# =========================
-# OWNER ID
-# =========================
-
-OWNER_ID = 1327719064   # <-- PUT YOUR TELEGRAM ID HERE
-
-# =========================
-# GLOBAL COMMAND FIREWALL
-# =========================
-
-@client.on(events.NewMessage(incoming=True))
-async def command_firewall(event):
-
-    # allow owner commands
-    if event.sender_id == OWNER_ID:
-        return
-
-    text = event.raw_text
-
-    if not text:
-        return
-
-    # block command prefixes
-    if text.startswith(".") or text.startswith("!") or text.startswith("/"):
-
-        raise events.StopPropagation
-
-
-# =========================
-# LOAD PLUGINS
-# =========================
-
+# load plugins
 for file in os.listdir("./plugins"):
     if file.endswith(".py"):
         importlib.import_module(f"plugins.{file[:-3]}")
 
-
-# =========================
-# START BOT
-# =========================
-
 client.start()
 
-print("Userbot running with plugins...")
+print("🔥 Clean Userbot Running...")
 
 client.run_until_disconnected()
