c94275f94eaef615dcabaadf9927a79ac78f522d
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-03-22 12:34:33 +0800

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,9 +1,23 @@
 from command import cmd
 import os
 import sys
+import database
+
+HELP = {
+    "name": "Restart",
+    "description": "Restart the bot",
+    "usage": ".restart"
+}
+
 
 @cmd("restart")
 async def restart(event):
+
+    # save message info
+    database.set("restart", {
+        "chat_id": event.chat_id,
+        "msg_id": event.id
+    })
 
     await event.reply("🔄 Restarting...")
 
