019bf64252656171e0b6956f6451f4418879f17c
quinzondavebrian2499-crypto <quinzondavebrian.2499@gmail.com>
2026-04-02 01:22:29 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 import database
 
 # Telegram API
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-
+API_ID = 2127709
+API_HASH = "5b7a6b1e4ff43ac9437114de58cef95a"
+OPENAI_API_KEY = "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"
 
 # Owner
 OWNER_ID = 1327719064  # your Telegram ID
